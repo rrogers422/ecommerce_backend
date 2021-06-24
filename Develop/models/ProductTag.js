cb77ba1193405c25577517819a37f2ca87b2df30
rrogers422 <Reagan422440@gmail.com>
2021-06-24 16:43:33 -0400

    updated productTag model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: "id",
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: "id",
+      }
+    },
   },
   {
     sequelize,
@@ -16,5 +36,4 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
-
 module.exports = ProductTag;
