4aadc2fe67aab5d38c888282e30c9c32328bdb75
rrogers422 <Reagan422440@gmail.com>
2021-06-24 16:32:27 -0400

    defined category columns/post route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -51,6 +51,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCatInfo => res.json(dbCatInfo))
+  .catch(err => {
+    console.log(err);
+    res.status(500).jsom(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
