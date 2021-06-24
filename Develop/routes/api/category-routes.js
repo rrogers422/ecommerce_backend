96201ecb255d38ea0f32d5c742a07634322d8446
rrogers422 <Reagan422440@gmail.com>
2021-06-24 17:10:21 -0400

    working on product routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -24,10 +24,10 @@ router.get('/', (req, res) => {
     res.sendStatus(500).json(err)
   })
 });
-
-router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', (req, res) => {
+
   Category.findOne({
     where: {
       id: req.params.id
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -2,11 +2,26 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
-// get all products
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [{
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    })
+    .then(dbProductInfo => res.json(dbProductInfo))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
